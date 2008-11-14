e1d3ee04dde4923bb3f24519595e3f341345ff82
SHolden <sholden@lamar.com>
2008-11-13 18:56:35 -0600

    commiting init

---
+++ init.rb
@@ -0,0 +1,9 @@
+require 'redmine'
+require_dependency 'changeset_patch'
+
+Redmine::Plugin.register :redmine_redmine_diff_email do
+  name 'Redmine Redmine Diff Email plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+end
